eef1775fc42aa0cc003c1ee9635bce1da0117a31
coryp4 <cory@burtano.com>
2020-09-18 10:48:11 -0700

    callbacks done

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -22,18 +22,23 @@ The zoos want to display both the scientific name and the animal name in front o
 */
 const displayNames = [];
 
-zooAnimals.forEach(nameThing);
+zooAnimals.forEach(function(zooAnimals){
+  console.log(`Name:${zooAnimals.animal_name}, Scientific:${zooAnimals.scientific_name}.`)
+  return `Name:${zooAnimals.animal_name}, Scientific:${zooAnimals.scientific_name}.`
+});
 
-
-console.log(displayNames);
+console.log(displayNames)
 
 /* Request 2: .map()
 
 The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
-
 */
 
-//const lowCaseAnimalNames
+
+
+//const lowCaseAnimalNames = zooAnimals.map()
+
+
 //console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -62,6 +67,10 @@ console.log(populationTotal);
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume (a, b, cb){
+  return cb(a, b);
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -69,11 +78,23 @@ console.log(populationTotal);
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+const add = function(a, b){
+  return a + b
+}
+
+const multiply = function(a, b){
+  return a * b
+}
+
+const greeting = function(a, b){
+  return `Hello ${a} ${b}, nice to meet you!`
+}
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+ console.log(consume(2, 2, add)); // 4
+ console.log(consume(10, 16, multiply)); // 160
+ console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
